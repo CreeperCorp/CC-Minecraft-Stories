a12d7ae9e5295e438dbddd73997d26a1ff7b65bb
ECRAEnnard <ennardcreep@gmail.com>
2025-10-11 01:36:55 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,19 @@
+print("Stories: God Of War, Imperceptible Manipulation")
+
+storyFile = input("What Series Do You Want To See?: ")
+
+if (storyFile = 'God Of War'):
+  print("Episode 1: The Way All Servers Start (Description: Never Trust An Ares)(1 Hour)")
+  print("Episode 2: Flamefrags (Description: Never Trust An Ares)(3 Hours)")
+  print("Episode 3: Physiatrist (Description: Never Trust An Ares)(3 Hours)")
+  print("Episode 4: Team Bing (Description: Never Trust An Ares)(4 Hours)")
+  print("Episode 5: The Invisible Mafia (Description: Never Trust An Ares)(5 Hours)")
+  print("Episode 6: The One Who Doesn't Speak (Description: Never Trust An Ares)(2 Hours)")
+  print("Episode 7: The Tunnel Rats (Description: Never Trust An Ares)(4 Hours)")
+  print("Episode 8: The Wonder (Description: Never Trust An Ares)(6 Hours)")
+  print("Episode 9: The Aspect Of Pure Evil (Description: Never Trust An Ares)(9 Hours)")
+
+GOW
+
+
+  
