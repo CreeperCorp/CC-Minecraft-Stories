b485562d21686f208a08c60a51535880bb8c906f
CreepSplotion <dekaiyondavis8@gmail.com>
2025-10-22 17:24:24 -0400

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ print("Stories: God Of War, Imperceptible Manipulation")
 storyFile = input("What Series Do You Want To See?: ")
 
 if (storyFile = 'God Of War'):
+  print("God Of War is a story about Anchor, a Minecraft noob overall, joining the Wonderous SMP for Season 2, and truly discovering what makes this SMP so 'Wonderful'.")
   print("Episode 1: The Way All Servers Start (Description: Never Trust An Ares)(1 Hour)")
   print("Episode 2: Flamefrags (Description: Never Trust An Ares)(3 Hours)")
   print("Episode 3: Physiatrist (Description: Never Trust An Ares)(3 Hours)")
@@ -14,10 +15,15 @@ if (storyFile = 'God Of War'):
   print("Episode 9: The Aspect Of Pure Evil (Description: Never Trust An Ares)(9 Hours)")
 
 elif (storyFile = 'Imperceptible Manipulation'):
+  print("Imperceptible Manipulation is a prequal to 'God Of War', which takes place during Season 1 of the Wonderous SMP, about how CreepSplotion came to be.")
   print("Episode 1: Humble Beginnings (Description: How It All Started)(3 Hours)")
   print("Episode 2: The Last Sign Of Immortality (Description: He's Too Good)(5 Hours)")
   print("Episode 3: Triple Threat (Description: There's three too many!)(2 Hours)")
   print("Episode 4: A British Betrayal (Description: The Second American Revolution)(4 Hours)")
+  print("Episode 5: Heartless (Description: He Lives Off Others Blood)(3 Hours)")
+  print("Episode 6: The First Ever Wonder (Description: It's Too Powerful)(6 Hours")
+  print("Episode 7: The True Wonderous Experience (Description: Rewarding The Immortal By Making Them More Immortal)(7 Hours")
+  print("Episode 8: No Regrets (Description: Once You Go Black, You Can't Go Back)(8 Hours")
 
 
   
