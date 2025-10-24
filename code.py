bc1200ce74dda6d5b5bd8f5baddb530a28e18c12
CreepSplotion <dekaiyondavis8@gmail.com>
2025-10-24 11:48:46 -0400

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ print("Stories: God Of War, Imperceptible Manipulation")
 
 storyFile = input("What Series Do You Want To See?: ")
 
-if (storyFile = 'God Of War'):
+if (storyFile == 'God Of War'):
   print("God Of War is a story about Anchor, a Minecraft noob overall, joining the Wonderous SMP for Season 2, and truly discovering what makes this SMP so 'Wonderful'.")
   print("Episode 1: The Way All Servers Start (Description: Never Trust An Ares)(1 Hour)")
   print("Episode 2: Flamefrags (Description: Never Trust An Ares)(3 Hours)")
@@ -14,16 +14,35 @@ if (storyFile = 'God Of War'):
   print("Episode 8: The Wonder (Description: Never Trust An Ares)(6 Hours)")
   print("Episode 9: The Aspect Of Pure Evil (Description: Never Trust An Ares)(9 Hours)")
 
-elif (storyFile = 'Imperceptible Manipulation'):
+elif (storyFile == 'Imperceptible Manipulation'):
   print("Imperceptible Manipulation is a prequal to 'God Of War', which takes place during Season 1 of the Wonderous SMP, about how CreepSplotion came to be.")
   print("Episode 1: Humble Beginnings (Description: How It All Started)(3 Hours)")
   print("Episode 2: The Last Sign Of Immortality (Description: He's Too Good)(5 Hours)")
   print("Episode 3: Triple Threat (Description: There's three too many!)(2 Hours)")
   print("Episode 4: A British Betrayal (Description: The Second American Revolution)(4 Hours)")
   print("Episode 5: Heartless (Description: He Lives Off Others Blood)(3 Hours)")
-  print("Episode 6: The First Ever Wonder (Description: It's Too Powerful)(6 Hours")
-  print("Episode 7: The True Wonderous Experience (Description: Rewarding The Immortal By Making Them More Immortal)(7 Hours")
-  print("Episode 8: No Regrets (Description: Once You Go Black, You Can't Go Back)(8 Hours")
+  print("Episode 6: The First Ever Wonder (Description: It's Too Powerful)(6 Hours)")
+  print("Episode 7: The True Wonderous Experience (Description: Rewarding The Immortal By Making Them More Immortal)(7 Hours)")
+  print("Episode 8: No Regrets (Description: Once You Go Black, You Can't Go Back)(8 Hours)")
+  print("Episode 9: A Visit To Hell (Description: This isn't Lucifer's domain anymore)(9 Hours)")
+
+elif (storyFile == 'The Wonderous Vault'):
+  print("The Wonderous Vault is a sequal to 'God Of War', taking placing during Season 3 of the Wonderous SMP. From the POV of Boosfer, find out what happened to CreepSplotion and uncover the lost history of this smp.")
+  print("Epsidoe 1: Capitalism (Description: What He Left Behind)(2 Hours)")
+  print("Episode 2: CreeperCorp (Description: A World Left In Terror)(3 Hours)")
+  print("Episode 3: Not A Wonderful Time (Description: Oh Baablu)(5 Hours)")
+  print("Episode 4: Looking For A Player Nobody Wants Back (Description: Where Did You Go?)(6 Hours)")
+  print("Episode 5: I Wonder Where He Could Be (Description: Who Created The Wonders?)(4 Hours)")
+  print("Episode 6: The UnScr3pted SMP (Description: He Who Has Done It All)(2 Hours)")
+  print("Episode 7: I See You (Description: You Can't Run Forever)(4 Hours)")
+  print("Episode 8: The Dawn Of Life (Description: As the Wonderous SMP Season 3 comes to a close, Boosfer realizes who the true owner of this server is, and CreepSplotion's about to kill him.)(5 Hours)")
+  print("Episode 9: A Defense Incapable Of Defending (Description: The Clock Is Ticking)(5 Hours)")
+
+elif (storyFile == 'A Wonderous Past'):
+  print("After discovering that CreepSplotion was about to kill UnScr3pted, the owner of the Wonderous SMP, and ban him for the next 3 seasons, Boosfer travels back in time before Season 1, to discover a way to stop it.")
+  print("Episode 1:  The First First Ever Wonder (Description: Is It Truly Scripted, If There's No Script?)(7 Hours)
+  
+
 
 
   
